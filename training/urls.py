781e529c3811f001b7d457356f5f6b27117f92dc
peng727336957 <727336957@qq.com>
2019-03-20 17:02:18 +0800

    路由器添加登录

--- training/urls.py
+++ training/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auto_views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auto_views.LoginView.as_view(), name='login')
+
 ]
